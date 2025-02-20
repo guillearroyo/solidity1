68f2e57314c114cae92ed3a5b124bf0553da9639
guillearroyo <guillermoizquierdoarroyo@gmail.com>
2025-02-20 16:08:28 +0100

    prueba

---
+++ contracts/HolaMundo.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.3;
+
+contract HolaMundo {
+    string private mensaje;
+
+    constructor(string memory _mensajeInicial) {
+        mensaje = _mensajeInicial;
+    }
+
+    function getMensaje() public view returns (string memory) {
+        return mensaje;
+    }
+
+    function setMensaje(string memory _nuevoMensaje) public {
+        mensaje = _nuevoMensaje;
+    }
+}
